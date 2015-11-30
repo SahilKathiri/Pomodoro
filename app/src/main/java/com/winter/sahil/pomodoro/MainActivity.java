d068cccae5115a01368e7d3a408678793bf4025b
Mohammed Sahil <sahilmohdsahil126@gmail.com>
2015-11-30 10:32:06 +0400

    Reverted back to when it worked

--- app/src/main/java/com/winter/sahil/pomodoro/MainActivity.java
+++ app/src/main/java/com/winter/sahil/pomodoro/MainActivity.java
@@ -1,24 +1,26 @@
 package com.winter.sahil.pomodoro;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 
 import at.grabner.circleprogress.CircleProgressView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    PomoTimer pomoTimer;
-    //    private Timer timer;
+    private Timer timer;
     private Boolean timer_flag = false;
     private long startTime = 120000L;
     private long interval = 50L;
     private long millisRemaining;
-    private ImageButton pomodoro, rest, longRest;
+    private Button start, reset;
     private TextView timerText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,76 +29,54 @@ public class MainActivity extends AppCompatActivity {
 
         final CircleProgressView circleProgress = (CircleProgressView) findViewById(R.id.circleProgress);
 
-        circleProgress.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                reset();
-                return true;
-            }
-        });
+        circleProgress.setMaxValue(startTime);
+        int secs = (int) (startTime / 1000);
+        int mins = secs / 60;
+        secs = secs % 60;
+        circleProgress.setText("" + mins);
+        circleProgress.setUnit("" + secs);
 
 
-        pomodoro = (ImageButton) findViewById(R.id.pomodoro);
-        rest = (ImageButton) findViewById(R.id.rest);
-        longRest = (ImageButton) findViewById(R.id.longRest);
+        start = (Button) findViewById(R.id.startButton);
+        start.setOnClickListener(this);
 
-        pomoTimer = new PomoTimer(this, circleProgress, pomodoro, rest, longRest);
-
-//        pomodoro.setOnClickListener(this);
-
-        pomodoro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pomoTimer.pomodoro();
-            }
-        });
-
-        rest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pomoTimer.rest();
-            }
-        });
-
-        longRest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pomoTimer.longRest();
-            }
-        });
-
+        reset = (Button) findViewById(R.id.reset_button);
 
         timerText = (TextView) findViewById(R.id.timer);
-//        timer = new Timer(startTime, interval);
+
+        timer = new Timer(startTime, interval);
         timerText.setText("" + startTime);
+
+
+
 
 
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        if (!timer_flag) {
-//            if (millisRemaining == 0) {
-//                timer = new Timer(startTime, interval);
-//            } else {
-//                timer = new Timer(millisRemaining, interval);
-//            }
-//            pomodoro.setImageResource(R.drawable.ic_pause_black_48dp);
-//            timer.start();
-//            timer_flag = true;
-//
-//        } else {
-//            timer.cancel();
-//            timer_flag = false;
-//            pomodoro.setImageResource(R.drawable.ic_play_arrow_black_48dp);
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        if (!timer_flag) {
+            if (millisRemaining == 0) {
+                timer = new Timer(startTime, interval);
+            } else {
+                timer = new Timer(millisRemaining, interval);
+            }
+            start.setText("Pause");
+            timer.start();
+            timer_flag = true;
 
-    private void reset() {
-        pomoTimer.done();
-        pomoTimer.cancel();
+        } else {
+            timer.cancel();
+            timer_flag = false;
+            start.setText("Resume");
+        }
+    }
+
+    public void reset(View view) {
+        timer.done();
+        timer.cancel();
         timer_flag = false;
-        pomodoro.setImageResource(R.drawable.ic_play_arrow_black_48dp);
+        start.setText("Start");
         CircleProgressView circleProgressView = (CircleProgressView) findViewById(R.id.circleProgress);
         int secs = (int) (startTime / 1000);
         int mins = secs / 60;
@@ -108,42 +88,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    class Timer extends CountDownTimer {
-//
-//        public Timer(long millisInFuture, long countDownInterval) {
-//            super(millisInFuture, countDownInterval);
-//        }
-//
-//        public void done() {
-//            onTick(0);
-//        }
-//
-//        @Override
-//        public void onTick(long millisUntilFinished) {
-//            millisRemaining = millisUntilFinished;
-//            timerText.setText("" + millisUntilFinished);
-//            CircleProgressView circleProgress = (CircleProgressView) findViewById(R.id.circleProgress);
-//            circleProgress.setValue(millisUntilFinished);
-//            int secs = (int) (millisUntilFinished / 1000);
-//            int mins = secs / 60;
-//            secs = secs % 60;
-//            circleProgress.setShowUnit(true);
-//            circleProgress.setAutoTextSize(false);
-//            circleProgress.setText(String.format("%d", mins));
-//            circleProgress.setUnit(String.format("%d", secs));
-//        }
-//
-//        @Override
-//        public void onFinish() {
-//            Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-//            timerText.setText("Finished");
-//            CircleProgressView circleProgress = (CircleProgressView) findViewById(R.id.circleProgress);
-//            circleProgress.setValue(0);
-//            circleProgress.setAutoTextSize(false);
-//            circleProgress.setText("Done");
-//            circleProgress.setShowUnit(false);
-//            v.vibrate(500);
-//        }
-//    }
+    class Timer extends CountDownTimer {
+
+        long millisInFuture;
+
+        public Timer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        public void done() {
+            onTick(0);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            millisRemaining = millisUntilFinished;
+            timerText.setText("" + millisUntilFinished);
+            CircleProgressView circleProgress = (CircleProgressView) findViewById(R.id.circleProgress);
+            circleProgress.setValue(millisUntilFinished);
+            int secs = (int) (millisUntilFinished / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            circleProgress.setShowUnit(true);
+            circleProgress.setAutoTextSize(false);
+            circleProgress.setText(String.format("%d", mins));
+            circleProgress.setUnit(String.format("%d", secs));
+        }
+
+        @Override
+        public void onFinish() {
+            Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            timerText.setText("Finished");
+            CircleProgressView circleProgress = (CircleProgressView) findViewById(R.id.circleProgress);
+            circleProgress.setValue(0);
+            circleProgress.setAutoTextSize(false);
+            circleProgress.setText("Done");
+            circleProgress.setShowUnit(false);
+            v.vibrate(500);
+        }
+    }
 }
 
